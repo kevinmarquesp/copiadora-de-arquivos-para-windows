9e25aa124fb3f133d93ec36fc03df17264924b8f
kevin <kevinmarqueskmp@gmail.com>
2021-05-14 09:39:24 -0300

    Opção de salvar informações em um arquivo adicionada com sucesso

--- copiadora-de-arquivos.bat
+++ copiadora-de-arquivos.bat
@@ -20,9 +20,10 @@ ECHO     Saia a qualquer momento com [Ctrl+C] ou use a espacos vazios como INPUT
 :: Força um erro que faz o programa parar a execução...
 ECHO.
 ECHO         - Digite [1] para alterar a origem e o destino.
-ECHO         - Digite [2] para ver os diretorios e arquivos da origem.
-ECHO             - Digite [A] para ver as subpastas em arvore da origem.
-ECHO             - Digite [B] para ver as subpastas e arquivos em arvore da origem.
+ECHO         - Digite [2] para salvar algumas informacoes da origem.
+ECHO             - Digite [A] para ver os diretorios e arquivos da origem.
+ECHO             - Digite [B] para ver as subpastas em arvore da origem.
+ECHO             - Digite [C] para ver as subpastas e arquivos em arvore da origem.
 ECHO         - Digite [3] para tomar acao rapidamente (inclui pastas vazias).
 ECHO         - Digite [4] para selecionar quais arquivos e pastas serao selecionados.
 ECHO.
@@ -34,12 +35,14 @@ SET /p userUM="|   O que deseja fazer?   |  %TIME:~0, -3%   > "
 
 
 IF %userUM% EQU 1 ( GOTO origemDestino )
-IF %userUM% EQU 2 ( GOTO diretoriosArquivos )
+IF %userUM% EQU 2 ( GOTO salvarInformacoes )
 
 IF %userUM% EQU A ( SET userUM=a )
 IF %userUM% EQU B ( SET userUM=b )
+IF %userUM% EQU C ( SET userUM=c )
 IF %userUM% EQU a ( GOTO diretoriosArquivos )
 IF %userUM% EQU b ( GOTO diretoriosArquivos )
+IF %userUM% EQU c ( GOTO diretoriosArquivos )
 
 IF %userUM% EQU 3 ( ECHO 3 )
 IF %userUM% EQU 4 ( ECHO 4 )
@@ -64,13 +67,35 @@ ECHO         Deixe o campo vazio caso nao queira alterar nada.
 ECHO.
 
 :: --- input ---
-SET  /p origem="|   Redefinir a  origem   |  %TIME:~0, -3%   > "
+SET /p origem="|   Redefinir a  origem   |  %TIME:~0, -3%   > "
 SET /p destino="|   Redefinir o destino   |  %TIME:~0, -3%   > "
 
 GOTO menu
 
 
 
+
+
+:salvarInformacoes
+CLS
+COLOR A
+TITLE Copiadora de arquivos   I   SALVAR INFORMACOES DA ORIGEM
+ECHO.
+
+:: --- input ---
+SET nameUM=%random%.txt
+SET /p nameUM="|   Nome do arquivo   |  %TIME:~0, -3%   > "
+
+ECHO ------------------------------------------------------------------------------- >%nameUM%
+DIR %origem% >>%nameUM%
+ECHO ------------------------------------------------------------------------------- >>%nameUM%
+TREE /f %origem% >>%nameUM%
+
+ECHO Informacoes salvas no arquivo %nameUM% com sucesso!
+ECHO.
+
+PAUSE
+GOTO menu
 
 
 :diretoriosArquivos
@@ -80,10 +105,10 @@ TITLE Copiadora de arquivos   I   MOSTRAR CONTEUDO DA ORIGEM
 
 ECHO.
 ECHO -------------------------------------------------------------------------------
-IF %userUM% EQU 2 (
+IF %userUM% EQU a (
 	DIR %origem%
 ) ELSE (
-	IF %userUM% EQU a (
+	IF %userUM% EQU b (
 		TREE %origem%
 	) ELSE (
 		TREE /f %origem%
@@ -93,11 +118,3 @@ ECHO ------------------------------------ Precione qualquer tecla para continuar
 
 PAUSE >nul
 GOTO menu
-
-
-
-
-
-:: Finalização do programa. Apenas para maior controle...
-echo [precione qualquer tecla para sair]
-pause >nul
